bd5a5997ab50ebb556fe6f1a982d9d434b308efc
jun960303 <rhkddnjs7878@naver.com>
2025-12-03 09:41:59 +0900

    jpa 최신

--- src/main/java/com/example/jpa/repository/StudentRepository.java
+++ src/main/java/com/example/jpa/repository/StudentRepository.java
@@ -2,10 +2,10 @@ package com.example.jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.jpa.entity.student;
+import com.example.jpa.entity.Student;
 
-// DAO역활
-// 기본적인 CRUD 메소드는 이미 정의되어 있다.
-public interface StudentRepository extends JpaRepository<student,Long>{
-
+// DAO 역할
+// 기본적인 CRUD 메소드는 이미 정의되어 있음
+public interface StudentRepository extends JpaRepository<Student,Long>{
+    
 }
--- src/test/java/com/example/jpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/jpa/repository/StudentRepositoryTest.java
@@ -1,29 +1,87 @@
 package com.example.jpa.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.example.jpa.entity.student;
+import com.example.jpa.entity.Student;
+import com.example.jpa.entity.constant.Grade;
 
 @SpringBootTest
 public class StudentRepositoryTest {
+    
+    @Autowired
+    private StudentRepository studentRepository;
 
-  private StudentRepository studentRepository;
 
-  @Test
-  public void insertTest() {
-    student student1 = student.builder()
-    .name("김희선")
-    .addr("부산")
-    .gender("null")
+    @Test
+    public void deleteTest(){
+        // Student student = studentRepository.findById(1L).get(); 
+        // studentRepository.delete(student);
+        studentRepository.deleteById(1L);
+    }
+
+
+    @Test
+    public void readTest(){
+        Student student = studentRepository.findById(2L).get();  
+        System.out.println(student); // com.example.jpa.entity.Student@63541960
+    }
+
+    @Test
+    public void readTest2(){
+        // 전체 학생 조회
+        List<Student> students = studentRepository.findAll();
+
+        for (Student student : students) {
+            System.out.println(student);
+        }
+    }
+
+
+
+
+    @Test
+    public void updateTest(){
+        // Entity
+        // update student set 수정컬럼=값 where id=1;
+
+        Optional<Student> result = studentRepository.findById(1L); 
+        Student student = result.get();
+        // student.changeName("성춘향");       
+        student.changeGrade(Grade.FRESHMAN);
+
+        // insert(c), update(u) 작업 시 호출
+        studentRepository.save(student);        
+    }
+
+
+
+    @Test
+    public void insertTest(){
+        // Entity
+        Student student = Student.builder()
+        .name("성춘향")
+        .addr("서울")
+        .gender("F")   
+        .grade(Grade.SENIOR)     
         .build();
 
-    studentRepository.save(student1);
-    // delete from ~ 호출
-    // studentRepository.delete(student);
-    // studentRepository.deleteById(student);
+        // insert(c), update(u) 작업 시 호출
+        studentRepository.save(student);
 
-    // select * from where id = 1;
-    //
-  }
+        // delete from ~ 호출
+        // studentRepository.delete(student);
+        // studentRepository.deleteById(student);
+
+        // select * from  where id = 1;
+        // studentRepository.findById(null);
+        // select * from  ;
+        // studentRepository.findAll();
+    }
+    
+
 }
